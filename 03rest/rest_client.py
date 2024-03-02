260464eb02c8b739ce93ba751f62e8cb509a62e5
KDanaAsturizagaT <kasturizagat@fcpn.edu.bo>
2024-03-01 22:41:10 -0400

    rest

--- 03rest/rest_client.py
+++ 03rest/rest_client.py
@@ -5,6 +5,7 @@ url = "http://localhost:8000/"
 ruta_get = url + "lista_estudiantes"
 get_response = requests.request(method="GET", url=ruta_get)
 print(get_response.text)
+
 # POST agrega un nuevo estudiante por la ruta /agrega_estudiante
 ruta_post = url + "agrega_estudiante"
 nuevo_estudiante = {
